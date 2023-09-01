01cd771e86339e18a9d6e400e604e74020e0d18d
savalisingh <savalidsingh@gmail.com>
2023-09-01 22:18:06 +0530

    1st code challage done

---
+++ 1stday/reverse_string.js
@@ -0,0 +1,3 @@
+let e="savali";
+let d=e.split("").reverse().join("");
+console.log('reverse of string savali',d)
---
+++ 1stday/reversearray.js
@@ -0,0 +1,7 @@
+let b=3;
+let c=5;
+console.log(b/c+2);
+
+const a=[1,2,3]
+reversed=a.reverse();
+console.log('reversed:',reversed);
